e529434f79e0bc81f17c0705a0c0fcd9dde4953c
minu <sh3fl@naver.com>
2021-02-01 21:31:00 +0900

    기초 웹구성 및 검색 api 구현완료

--- Shophelper/views.py
+++ Shophelper/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+import requests as req
+import json
+import key
 # Create your views here.
+
+def search(request) :
+    if request.method == "POST" :
+        keyword = request.POST.get("keyword")
+
+        X_Naver_Client_Id = getattr(key, 'X_Naver_Client_Id')
+        X_Naver_Client_Secret = getattr(key, 'X_Naver_Client_Secret')
+
+        url ="https://openapi.naver.com/v1/search/shop.json?query={0}&display=10&start=1&sort=sim".format(keyword)
+        headers = {
+            "X-Naver-Client-Id": X_Naver_Client_Id,
+            "X-Naver-Client-Secret":  X_Naver_Client_Secret,
+            "User-Agent" : "curl/7.49.1",
+            "Accept" : "*/*"
+        }
+
+        response=req.get(url= url, headers=headers)
+        msg=response.json()
+        
+        return render(request, 'search.html', {"msg" : msg})
+
+def main(request) :
+    if request.method == "GET" :
+      return render(request, 'main.html')
